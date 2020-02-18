264a94b0cd9a88e45531fbfd96e6220712cee9b1
Larissa Ellen Gomes Marques <larissa.marques@.stitdata.com>
2020-02-18 11:33:13 -0300

    up

--- 01_aula/config/express.js
+++ 01_aula/config/express.js
@@ -1,13 +1,14 @@
-var express = require("express");
-var home = require("../app/routes/home");
+var express = require('express');
+var home = require('../app/routes/home');
 
-module.exports = function(){
-    var app = express();
-    app.set('port', 3000);
+module.exports = function() {
+  var app = express();
+  var porta = process.env.PORT || 8080;
+  app.set('port', porta);
 
-    app.use(express.static('./public'));
-    app.set('view engine', 'ejs');
-    app.set('views', './app/views');
-    home(app);
-    return app;
-}
+  app.use(express.static('./public'));
+  app.set('view engine', 'ejs');
+  app.set('views', './app/views');
+  home(app);
+  return app;
+};
